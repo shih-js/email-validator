41ecb8523e11ae9c51ed76b33d27262e66222fb3
Jeff Shih <jeffshih@outlook.com>
2020-05-20 15:53:55 -0700

    Remove old import.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import emailReducer from '../components/EmailForm/emailSlice';
 
 export const store = configureStore({
 	reducer: {
-		counter: counterReducer,
 		email: emailReducer,
 	},
 });
